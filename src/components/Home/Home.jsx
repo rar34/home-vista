c125ce4a43aa03b31413d9e614537cb1c9d56e5d
rar34 <ra.cse34@gmail.com>
2024-04-16 17:57:36 +0600

    added banner using swipper slider

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import Estate from "./Estate";
+import Banner from "./Banner";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
 
     return (
         <div>
+            <Banner></Banner>
             <h2 className="text-3xl font-bold text-center my-10">Our Estates</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10">
                 {
