f5f853d34b0f8e8a63e1de94a8d96f6c86ff47f8
rar34 <ra.cse34@gmail.com>
2024-04-16 23:49:17 +0600

    added estate details

--- src/components/Home/EstateDetails.jsx
+++ src/components/Home/EstateDetails.jsx
@@ -17,12 +17,26 @@ const EstateDetails = () => {
         }
     }, [estates, idInt]);
 
-    const { estate_title, image, status, description, segment, area, price } = singleEstate || {};
+    const { estate_title, image, status, description, segment, area, price, location, facilities } = singleEstate || {};
 
     return (
-        <div className='text-white'>
-            <h2 className="text-3xl">estates details here of {id}</h2>
-            <p>title {estate_title}</p>
+        <div className="hero my-6 bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <img src={image} className=" lg:h-[420px] rounded-lg shadow-lg" />
+                <div>
+                    <h4 className="text-2xl mb-2 font-semibold text-gray-500">Available for: <span className='text-[#23BE0A]'>{status}</span></h4>
+                    <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold">{estate_title}</h1>
+                    <p className='font-semibold my-4'><span>Segment: </span> {segment}</p>
+                    <hr />
+                    <p className="flex gap-4 flex-col md:flex-row my-2 items-center font-bold">Facilities: {
+                        facilities?.map((facility, idx) => <span className="text-[#23BE0A] bg-[#23BE0A0D] px-4 py-2" key={idx}>{facility}</span>)
+                    }</p>
+                    <hr />
+                    <p className='font-medium'><span className='font-bold'>Location:</span> {location}</p>
+                    <p className="py-6">{description}</p>
+                    <p className='font-semibold flex gap-14 text-gray-600'><span>Area: {area}</span> <span>Price: {price}</span></p>
+                </div>
+            </div>
         </div>
     );
 };
